ee0345ec02510321f32227ed13c7083d114fcdc5
camellecastro <camelleccastro@gmail.com>
2024-04-24 22:08:09 -0600

    changes

--- backend/app.js
+++ backend/app.js
@@ -6,15 +6,15 @@ const professionalRoutes = require('./routes/professional');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const port = process.env.PORT || 8080;
 const app = express();
+const port = process.env.PORT || 8080;
 
-app.use(bodyParser.json());
-app.use((req, res, next) => {
+app.use(bodyParser.json())
+    .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
-});
-app.use('/professional', professionalRoutes);
+})
+    .use('/professional', professionalRoutes);
 
 mongodb.initDb((err, mongodb) => {
     if (err) {
